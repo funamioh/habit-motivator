e5f9b214de6791dab6e11a50eefce7c22ea10357
Miho Funayama <funamiho1011@gmail.com>
2024-02-28 18:30:53 +0900

    added authentication functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,14 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def home
   end
 
   def about
-    @members = ["Miho", "Taka", "Tiger", "Vincent"]
+    @members = ['Miho', 'Taka', 'Tiger', 'Vincent']
 
     if params[:member]
-      @members = members.select {|member| member.start_with?(params[:member])}
+      @members = members.select { |member| member.start_with?(params[:member]) }
     end
   end
 end
